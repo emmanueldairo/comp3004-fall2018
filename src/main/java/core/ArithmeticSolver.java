e615c98d147383dd4adc65fcf354a5676df5ae73
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2018-09-10 15:38:34 -0400

    main: added arithmeticSolver class implementation

---
+++ src/main/java/core/ArithmeticSolver.java
@@ -0,0 +1,20 @@
+package core;
+
+public class ArithmeticSolver {
+	public double add(double left, double right) {
+		return left+right;
+	}
+	public double subtract(double left, double right) {
+		return left-right;
+	}
+	public double multiply(double left, double right) {
+		return left*right;
+	}
+	public double divide(double left, double right) {
+		return left/right;
+	}
+	public double modulus(double left, double right) {
+		return left%right;
+	}
+	
+}
