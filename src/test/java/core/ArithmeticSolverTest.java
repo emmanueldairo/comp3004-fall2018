832af15fc86ff26b43773697b4be7a142316f44f
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2018-09-10 15:19:26 -0400

    test: added arithmeticSolverTest code

--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -1,5 +1,29 @@
 package core;
 
-public class ArithmeticSolverTest {
+import junit.framework.TestCase;
 
+public class ArithmeticSolverTest extends TestCase{
+	
+	public void testAdd() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		assertEquals(4, solver.add(2,2));
+	}
+	
+	public void testSubtract() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		assertEquals(2, solver.subtract(4,2));
+	}
+	public void testMultiply() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		assertEquals(16, solver.multiply(8,2));
+	}
+	
+	public void testDivide() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		assertEquals(3, solver.divide(9,2));
+	}
+	public void testModulus() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		assertEquals(1, solver.modulus(5,2));
+	}
 }
