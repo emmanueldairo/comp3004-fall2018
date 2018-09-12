bbebffd1e9d2793e8775502815a703056cce19bf
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2018-09-12 14:00:15 -0400

    test: the solve method

--- src/main/java/core/ArithmeticSolver.java
+++ src/main/java/core/ArithmeticSolver.java
@@ -16,5 +16,29 @@ public class ArithmeticSolver {
 	public double modulus(double left, double right) {
 		return left%right;
 	}
+	public double solve(String operator, double leftBox, double rightBox) {
+		double answer=0;
+		switch (operator) {
+			case "+":
+				answer=add(leftBox, rightBox);
+				break;
+			case "-":
+				answer=subtract(leftBox, rightBox);
+				break;
+			case "x":
+				answer=multiply(leftBox, rightBox);
+				break;
+			case "/":
+				answer=divide(leftBox, rightBox);
+				break;
+			case "%":
+				answer=modulus(leftBox, rightBox);
+				break;
+	
+			default:
+				break;
+		}
+		return answer;
+	}
 	
 }
